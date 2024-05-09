f9d44fd496fc33bc58551df4dd48dc126e2f97e7
ezTxmMC <eztxmmc@hotmail.com>
2024-05-09 16:59:17 +0200

    created project and started with BetterEntity and BetterScoreboard

---
+++ src/main/java/eu/moonwriters/betterspigotlib/entity/BetterEntity.java
@@ -0,0 +1,33 @@
+package eu.moonwriters.betterspigotlib.entity;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+
+public class BetterEntity {
+    private final Entity entity;
+
+    public BetterEntity(Location location, EntityType type) {
+        this.entity = location.getWorld().spawnEntity(location, type);
+    }
+
+    public BetterEntity displayName(String displayName) {
+        entity.setCustomName(displayName);
+        entity.setCustomNameVisible(true);
+        return this;
+    }
+
+    public BetterEntity addPassenger(Entity passenger) {
+        entity.addPassenger(passenger);
+        return this;
+    }
+
+    public BetterEntity removePassenger(Entity passenger) {
+        entity.removePassenger(passenger);
+        return this;
+    }
+
+    public void remove() {
+        entity.remove();
+    }
+}
---
+++ src/main/java/eu/moonwriters/betterspigotlib/scoreboard/BetterScoreboard.java
@@ -0,0 +1,53 @@
+package eu.moonwriters.betterspigotlib.scoreboard;
+
+import org.bukkit.scoreboard.Criteria;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class BetterScoreboard {
+    private final Scoreboard scoreboard;
+    private final Objective objective;
+    private final Map<Integer, Team> lines;
+
+    public BetterScoreboard(Scoreboard scoreboard, String scoreboardId, String displayName) {
+        this.scoreboard = scoreboard;
+        this.objective = getObjective(scoreboardId, displayName);
+        this.lines = new HashMap<>();
+    }
+
+    public void setLine(int line, String content, boolean updatable) {
+        if (updatable) {
+            Team team = scoreboard.getTeam(String.valueOf(line));
+            if (team == null) {
+                team = scoreboard.registerNewTeam(String.valueOf(line));
+            }
+            team.setPrefix(content);
+            team.addEntry(UUID.randomUUID().toString());
+            objective.getScore(UUID.randomUUID().toString()).setScore(line);
+            lines.put(line, team);
+            return;
+        }
+        objective.getScore(content).setScore(line);
+    }
+
+    public void updateLine(int line, String newContent) {
+        if (!lines.containsKey(line)) {
+            return;
+        }
+        lines.get(line).setPrefix(newContent);
+    }
+
+    private Objective getObjective(String id, String display) {
+        AtomicReference<Objective> currentObjective = new AtomicReference<>(scoreboard.getObjective(id));
+        if (currentObjective.get() == null) {
+            currentObjective.set(scoreboard.registerNewObjective(id, Criteria.DUMMY, display));
+        }
+        return currentObjective.get();
+    }
+}
