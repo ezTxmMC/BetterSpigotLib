36515b657b7f034eed030f1e05ea49f50f866d39
ezTxmMC <eztxmmc@hotmail.com>
2024-05-31 15:04:57 +0200

    feat: remove BetterScoreboard

--- src/main/java/eu/moonwriters/betterspigotlib/scoreboard/BetterScoreboard.java
+++ src/main/java/eu/moonwriters/betterspigotlib/scoreboard/BetterScoreboard.java
@@ -13,12 +13,14 @@ import java.util.UUID;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class BetterScoreboard {
-    private final Scoreboard scoreboard;
-    private final Objective objective;
-    private final Map<Integer, Team> lines;
+    private Scoreboard scoreboard;
+    private String scoreboardId;
+    private Objective objective;
+    private Map<Integer, Team> lines;
 
     public BetterScoreboard(Player player, String scoreboardId, String displayName) {
         this.scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+        this.scoreboardId = scoreboardId;
         this.objective = getOrCreateObjective(scoreboardId, displayName);
         this.lines = new HashMap<>();
         player.setScoreboard(this.scoreboard);
@@ -44,6 +46,14 @@ public class BetterScoreboard {
             return;
         }
         lines.get(line).setPrefix(newContent);
+    }
+
+    public void remove() {
+        objective.unregister();
+        objective = null;
+        scoreboard = null;
+        scoreboardId = null;
+        lines = null;
     }
 
     private Objective getOrCreateObjective(String id, String display) {
